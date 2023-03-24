3442892b3f7430980bbcf44a5ad36d08026155e0
rosbel <rosbelcr@gmail.com>
2023-03-24 17:37:25 -0400

    Improving the UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,36 +36,49 @@ export default function HomePage() {
         
         <h1>Home Page</h1>
 
+    <div className='container'>
 
-     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        
-        <TableHead>
-          <TableRow>
-            <TableCell><h1>Title</h1></TableCell>
-            <TableCell align="right"><h3>Update</h3></TableCell>
-            <TableCell align="right"><h3>Delete</h3></TableCell>
-          </TableRow>
-        </TableHead>
-        
-        <TableBody>
-          {dataApi.map(({id, title}) => (
-            <TableRow
-              key={id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {title}
-              </TableCell>
-              <TableCell align="right">'Bttn Upd'</TableCell>
-              <TableCell align="right">'Bttn Dlt'</TableCell>
-              
+    <button className="btn btn-primary" type="button">Create Post</button>
+
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          
+          <TableHead>
+            <TableRow>
+              <TableCell><h1>Title</h1><p>{`${dataApi.length} results found`}</p></TableCell>
+              <TableCell align="right"><h3>Update</h3></TableCell>
+              <TableCell align="right"><h3>Delete</h3></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
+          </TableHead>
+          
+          <TableBody>
+            {dataApi.map(({id, title}) => (
+              <TableRow
+                key={id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {title}
+                </TableCell>
+                
+                <TableCell align="right">
+                  <button type="button" className="btn btn-outline-success">Update</button>
+                </TableCell>
+                
+                <TableCell align="right">
+                  <button type="button" className="btn btn-outline-danger">Delete</button>
+                </TableCell>
+                
+              </TableRow>
+            ))}
+          </TableBody>
 
-      </Table>
-    </TableContainer>
+        </Table>
+      </TableContainer>
+
+    </div>
+
 
         
     </MainLayout>
