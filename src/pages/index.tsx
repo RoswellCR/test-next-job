fbb98c6f32516f92414652515c579b7b251f92cf
rosbel <rosbelcr@gmail.com>
2023-03-24 18:49:52 -0400

    CRUD methods done without data from form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';  
 
 import { MainLayout } from '../components/layouts/MainLayout'
 import { Post } from '../interfaces/post';
@@ -18,17 +19,56 @@ export default function HomePage() {
 
   const [dataApi, setDataApi] = useState<Post[]>([]);
 
-  const endPoint = 'https://jsonplaceholder.typicode.com/posts';
+  const END_POINT = 'https://jsonplaceholder.typicode.com/posts';
 
   useEffect(() => {
     const getDataApi = async () =>{
-      const {data: res} = await axios.get(endPoint);
+      const {data: res} = await axios.get(END_POINT);
       setDataApi(res);
       console.log(res);
     }
     getDataApi();
   
   }, [])
+
+  const createPost = async () => {
+    const min = 100;
+    const max = 1000;
+    const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(randomNum)
+
+    const post:Post = {
+      title:'new test',
+      body: 'test',
+      id:randomNum,
+      userId:randomNum
+
+    }
+    
+    await axios.post(END_POINT, post);
+    setDataApi([post, ...dataApi])
+  }
+
+
+  const updatePost = async (id: number, title: string='testing title updated') => { 
+    await axios.put(`${END_POINT}/${id}`)
+    const tempDataApi = [...dataApi];
+    const index = tempDataApi.findIndex(post => post.id===id);
+    tempDataApi[index] = {
+      id,
+      userId: id,
+      title,
+      body:'test'
+    };
+    setDataApi(tempDataApi);
+  }
+
+  const deletePost = async (id: number) => {
+     
+    await axios.delete(`${END_POINT}/${id}`) 
+    
+    setDataApi(dataApi.filter(p=> p.id!==id));
+  }
   
 
   return (
@@ -38,7 +78,7 @@ export default function HomePage() {
 
     <div className='container'>
 
-    <button className="btn btn-primary" type="button">Create Post</button>
+    <button onClick={createPost} className="btn btn-primary" type="button">Create Post</button>
 
 
       <TableContainer component={Paper}>
@@ -63,11 +103,11 @@ export default function HomePage() {
                 </TableCell>
                 
                 <TableCell align="right">
-                  <button type="button" className="btn btn-outline-success">Update</button>
+                  <button onClick={()=>updatePost(id)} type="button" className="btn btn-outline-success">Update</button>
                 </TableCell>
                 
                 <TableCell align="right">
-                  <button type="button" className="btn btn-outline-danger">Delete</button>
+                  <button onClick={()=>deletePost(id)} type="button" className="btn btn-outline-danger">Delete</button>
                 </TableCell>
                 
               </TableRow>
