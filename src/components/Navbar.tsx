6b838a95befaf88dcc9f22d9f3aeafd07ec0ed72
rosbel <rosbelcr@gmail.com>
2023-03-24 15:51:27 -0400

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { ActiveLink } from './ActiveLink';
+import styles from './Navbar.module.css';
+
+
+
+export const Navbar = () => {
+  return (
+    <nav className={ styles['menu-container'] }>
+        <h1>TEST-NEXT-JOB</h1>
+    </nav>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+
+import Link from 'next/link'
+import { MainLayout } from '../components/layouts/MainLayout'
+
+
+export default function HomePage() {
+
+  return (
+    <MainLayout>
+        
+        <h1>Home Page</h1>
+        
+    </MainLayout>
+  )
+}
